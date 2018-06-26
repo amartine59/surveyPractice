340444f8267318b452ae9cbbe22f4d0c4f6927fa
Andres <barathum196@gmail.com>
2018-06-26 16:23:50 -0500

    It validates the age already but still needs to set an order to the appearance of each row

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ function hideConditionalBoxes() {
   document.getElementById("email-alert").style.display = "none";
 }
 //(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) regex for email validation
+var fifthRow = document.getElementById("fif-row");
+var sixthRow = document.getElementById("six-row");
+var seventhRow = document.getElementById("svn-row");
+var eighthRow = document.getElementById("eig-row");
 
 function validateRequiredInputs() {
   var name = document.getElementById("name-input");
@@ -19,27 +23,36 @@ function validateRequiredInputs() {
   var date = document.getElementById("date-selector");
 
   if (
-    name.value.trim() != "" &&
-    lName.value.trim() != "" &&
+    name.value.trim() != "" ||
+    lName.value.trim() != "" ||
     date.value.trim() != ""
   ) {
     if (email.value.trim() != "") {
-      if (
-        /^[a-zA-Z]+$/.test(name) &&
-        /^[a-zA-Z]+$/.test(lName) &&
-        /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-      ) {
+      if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
         return true;
       } else {
+        var error = "You must enter a valid email";
+        errorDeploy(email, error);
+        return false;
       }
     }
-    if (/^[a-zA-Z]+$/.test(name) && /^[a-zA-Z]+$/.test(lName)) {
-      return true;
-    } else {
-    }
+    return true;
+    //To display the errors on the empty boxes
+  } else if (name.value.trim() == "") {
+    var error = "Im afraid you must enter a name..";
+    errorDeploy(name, error);
   }
-}
+  if (lName.value.trim() == "") {
+    var error = "Sorry but this field cannot be blank..";
+    errorDeploy(lName, error);
+  }
 
+  if (date.value.trim() == "") {
+    displayDateAlert();
+  }
+  return false;
+}
+//Displaying the many possible errors
 function displayNameAlert(error) {
   document.getElementById("name-alert").style.display = "block";
   document.getElementById("name-alert").innerHTML = error;
@@ -54,13 +67,62 @@ function displayEmailAlert(error) {
   document.getElementById("email-alert").style.display = "block";
   document.getElementById("email-alert").innerHTML = error;
 }
+//Shakes the date selector to show that we require the date of birth
+function displayDateAlert() {
+  date.classList.add("error");
+  setTimeout(function() {
+    date.classList.remove("error");
+  }, 300);
+}
+
 //Launches the error related to the given element(input)
 function errorDeploy(boxElement, error) {
-  var selectedBox = boxId.id;
+  var selectedBox = boxElement.id;
   if (selectedBox == "name-input") {
     displayNameAlert(error);
+    return;
   }
   if (selectedBox == "l-name-input") {
     displayLNameAlert(error);
+    return;
+  }
+  if (selectedBox == "email-alert") {
+    displayEmailAlert(error);
+    return;
+  }
+}
+
+//Calculate the age given a date of birth
+function calculateAge(e) {
+  var currentDate = new Date();
+  var dateBirth = new Date(e.target.value);
+  var Age = currentDate.getUTCFullYear() - dateBirth.getUTCFullYear();
+  var monthDifference = currentDate.getUTCMonth() - dateBirth.getUTCMonth();
+  if (
+    monthDifference < 0 ||
+    (monthDifference == 0 && currentDate.getUTCDate() < dateBirth.getUTCDate())
+  ) {
+    Age--;
+  }
+
+  if (Age >= 18) {
+    displayBox(fifthRow);
+    displayBox(sixthRow);
+    displayBoxF();
+  } else if (Age < 18) {
+    displayBoxF();
+    displayBox(eighthRow);
+  }
+}
+function displayBox(boxElement) {
+  boxElement.style.display = "block";
+}
+
+function displayBoxF() {
+  document.getElementById("svn-row").style.display = "flex";
+}
+
+function onFilledForm() {
+  if (validateRequiredInputs) {
   }
 }
